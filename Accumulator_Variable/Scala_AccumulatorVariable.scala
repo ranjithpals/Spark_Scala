e886d6639c26dd1b9241493e4b29ee2aab67347e
Ranjith Gonugunta <39640906+ranjithpals@users.noreply.github.com>
2021-04-17 22:17:13 -0400

    Update Scala_AccumulatorVariable.scala

--- Accumulator_Variable/Scala_AccumulatorVariable.scala
+++ Accumulator_Variable/Scala_AccumulatorVariable.scala
@@ -9,13 +9,13 @@ object Scala_AccumulatorVariable extends App {
   
   val sc = new SparkContext("local[*]", "Accumulator")
   
-  //val accum = sc.longAccumulator("empty line accumulator")
-  var accum = 0
+  val accum = sc.longAccumulator("empty line accumulator")
+  //var accum = 0
   val input = sc.textFile("C:/Users/Owner/OneDrive/Documents/Big Data_TrendyTech/Week-10/Data/samplefile.txt")
   
-  //input.foreach(x => if(x=="") accum.add(1))
-  input.foreach(x=>if(x=="")accum += 1)
+  input.foreach(x => if(x=="") accum.add(1))
+  //input.foreach(x=>if(x=="")accum += 1)
   
-  //println(s"No of Blank lines in the file: $accum.value)")
-  println(accum)
+  println(s"No of Blank lines in the file: $accum.value)")
+  //println(accum)
 }
