fb42479b7428c6a8d66942048d96d0949c262f76
ranjithpals <ranjithpals@gmail.com>
2021-10-03 11:05:38 -0400

    Streaming example with Stateful Window and Sliding Interval

---
+++ Streaming/Stream_Stateful_wCount.scala
@@ -0,0 +1,48 @@
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+import org.apache.spark.SparkContext
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.Seconds
+
+
+object Stream_Stateful_wCount extends App {
+  
+  //Set Log level 
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  
+  //Create Spark Context
+  val sc = new SparkContext("local[*]", "Streaming Word Count")
+  
+  //Create a Spark Streaming context
+  val ssc = new StreamingContext(sc, Seconds(5))
+  
+  //Read the Stream from the Producer: nc -lk 9994
+  val baseRDD = ssc.socketTextStream("g01.itversity.com", 9994)
+  
+  //Apply transformation to flatten the Input
+  val rdd1 = baseRDD.flatMap(x=>x.split(" "))
+  
+  //Apply Transformation to convert each word to key-value pair
+  val rdd2 = rdd1.map(x=>(x, 1))
+  
+  ssc.checkpoint(".")
+  
+  def updateFunc(newValues: Seq[Int], previousState: Option[Int]): Option[Int] = {
+    //Adding the previous state's value
+    val newCount = previousState.getOrElse(0) + newValues.sum
+    Some(newCount)
+  }
+  
+  //Count the occurrences of each word using Stateful Transformation
+  val finalRDD = rdd2.updateStateByKey(updateFunc)
+  
+  //Display the word count
+  finalRDD.print()
+  
+  //Start listening to the stream
+  ssc.start()
+  
+  //Await Termination
+  ssc.awaitTermination()
+  
+}
